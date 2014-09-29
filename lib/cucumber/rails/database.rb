af16e65f7c7ee9048dc0a1a1f7157a2aa251d575
Dean Holdren <deanholdren@gmail.com>
2014-09-29 19:14:25 -0400

    Remove setting/resetting shared_connection on AR::Base

--- lib/cucumber/rails/database.rb
+++ lib/cucumber/rails/database.rb
@@ -91,7 +91,6 @@ module Cucumber
           # Forces all threads to share a connection on a per-model basis,
           # as connections may vary per model as per establish_connection. This works
           # on Capybara because it starts the web server in a thread.
-          ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
           ActiveRecord::Base.descendants.each do |model|
             model.shared_connection = model.connection
           end
@@ -99,7 +98,6 @@ module Cucumber
 
         def before_non_js
           # Do not use a shared connection unless we're in a @javascript scenario
-          ActiveRecord::Base.shared_connection = nil
           ActiveRecord::Base.descendants.each do |model|
             model.shared_connection = nil
           end
